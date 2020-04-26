763574e4f68307aee220f8af453f9753c3b909fd
Jörn Dinkla <joern@dinkla.net>
2020-04-26 16:32:48 +0200

    Remove unused resolutions

--- src/main/kotlin/net/dinkla/raytracer/utilities/Resolution.kt
+++ src/main/kotlin/net/dinkla/raytracer/utilities/Resolution.kt
@@ -1,20 +1,16 @@
 package net.dinkla.raytracer.utilities
 
+const val RATIO_16_TO_9 = 16.0 / 9.0
+
 data class Resolution(val hres: Int, val vres: Int) {
 
-    constructor(vres: Int) : this((vres * IMAGE_RATIO).toInt(), vres)
+    constructor(vres: Int) : this((vres * RATIO_16_TO_9).toInt(), vres)
 
     companion object {
-        const val IMAGE_RATIO = 16.0 / 9.0
-
-        val RESOLUTION_32 = Resolution(32)
-        val RESOLUTION_320 = Resolution(320)
-        val RESOLUTION_480 = Resolution(480)
         val RESOLUTION_720 = Resolution(720)
         val RESOLUTION_1080 = Resolution(1080)
         val RESOLUTION_1440 = Resolution(1440)
         val RESOLUTION_2160 = Resolution(2160)
         val RESOLUTION_4320 = Resolution(4320)
     }
-
 }
