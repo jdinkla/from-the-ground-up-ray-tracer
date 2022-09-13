fafbb8c26a3a533bf438666e70200e0cefeda982
Jörn Dinkla <joern@dinkla.net>
2022-09-13 14:59:41 +0200

    convert to multiplatform project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,57 +1,90 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
+val logbackVersion = "1.2.11"
 val kotestVersion = "5.4.2"
 val coroutinesVersion = "1.6.4"
-val logbackVersion = "1.2.11"
+val mockkVersion = "1.12.7"
 
-plugins {
-    kotlin("jvm") version "1.7.10"
-    id("io.gitlab.arturbosch.detekt").version("1.20.0")
-    idea
+group = "net.dinkla"
+version = "1.0"
+
+buildscript {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+plugins {
+    kotlin("multiplatform") version "1.7.10"
+    id("io.kotest.multiplatform") version "5.4.2"
+    application
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
     maven {
         url = uri("https://plugins.gradle.org/m2/")
     }
-    mavenCentral()
+    maven {
+        url = uri("https://kotlin.bintray.com/kotlinx")
+    }
 }
 
-val compileKotlin by tasks.getting(KotlinCompile::class) {
-    kotlinOptions.jvmTarget = "17"
+kotlin {
+    jvm()
+    js() {
+        nodejs()
+        binaries.executable()
+    }
+    linuxX64() {
+        binaries.executable()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation(kotlin("script-runtime"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+                implementation("io.kotest:kotest-framework-engine:$kotestVersion")
+            }
+        }
+        val jvmMain by getting {
+            dependsOn(commonMain)
+            dependencies {
+                implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:${coroutinesVersion}")
+            }
+        }
+        val jvmTest by getting {
+            dependsOn(commonTest)
+            dependencies {
+                implementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+                implementation("io.mockk:mockk:$mockkVersion")
+            }
+        }
+        val jsMain by getting {
+            dependsOn(commonMain)
+        }
+        val jsTest by getting {
+            dependsOn(commonTest)
+        }
+        val linuxX64Main by getting {
+            dependsOn(commonMain)
+        }
+        val linuxX64Test by getting {
+            dependsOn(commonTest)
+        }
+    }
 }
 
-val compileTestKotlin by tasks.getting(KotlinCompile::class) {
-    kotlinOptions.jvmTarget = "17"
-}
-
-tasks.withType<Test>().configureEach {
+tasks.named<Test>("jvmTest") {
     useJUnitPlatform()
-}
-
-detekt {
-    config = files("detekt-config.yml")
-    source = files("src/main/kotlin", "src/test/kotlin")
-
-}
-
-task<JavaExec>("commandline") {
-    main = "net.dinkla.raytracer.gui.CommandLineUi"
-    classpath = sourceSets["main"].runtimeClasspath
-    args = listOf("World10.kt", "build/World10.png")
-}
-
-task<JavaExec>("swing") {
-    main = "net.dinkla.raytracer.gui.swing.FromTheGroundUpRayTracerKt"
-    classpath = sourceSets["main"].runtimeClasspath
 }
