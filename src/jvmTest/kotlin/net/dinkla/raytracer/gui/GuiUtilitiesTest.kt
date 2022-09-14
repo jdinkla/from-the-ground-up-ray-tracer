a7feb1aec2a78e19a614a02e7743b17a5b7b80f8
Jörn Dinkla <jdinkla@thoughtworks.com>
2022-09-14 18:35:19 +0200

    various

--- src/jvmTest/kotlin/net/dinkla/raytracer/gui/GuiUtilitiesTest.kt
+++ src/jvmTest/kotlin/net/dinkla/raytracer/gui/GuiUtilitiesTest.kt
@@ -1,35 +1,31 @@
 package net.dinkla.raytracer.gui
 
-import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
-internal class GuiUtilitiesTest : AnnotationSpec() {
+internal class GuiUtilitiesTest : StringSpec({
 
-    @Test
-    fun `getOutputPngFileName #1`() {
+    "getOutputPngFileName #1" {
         val s = getOutputPngFileName("World73.groovy")
         s.substring(s.length - 11) shouldBe "World73.png"
     }
 
-    @Test
-    fun `getOutputPngFileName #2`() {
+    "getOutputPngFileName #2" {
         val s = getOutputPngFileName("ABC.World73.groovy")
         s.substring(s.length - 11) shouldBe "World73.png"
     }
 
-    @Test
-    fun `extractFileName should extract filename for Windows directories`() {
+    "extractFileName should extract filename for Windows directories" {
         val fileName =
             "C:\\workspace\\from-the-ground-up-ray-tracer\\src\\main\\kotlin\\net\\dinkla\\raytracer\\examples\\NewWorld3.kt"
         val directory = "C:\\workspace\\from-the-ground-up-ray-tracer\\src\\main\\kotlin\\net\\dinkla\\raytracer"
         extractFileName(fileName, directory, "\\") shouldBe "examples\\NewWorld3.kt"
     }
 
-    @Test
-    fun `extractFileName should extract filename for UNIX directories`() {
+    "extractFileName should extract filename for UNIX directories" {
         val fileName =
             "/workspace/from-the-ground-up-ray-tracer/src/main/kotlin/net/dinkla/raytracer/examples/NewWorld3.kt"
         val directory = "/workspace/from-the-ground-up-ray-tracer/src/main/kotlin/net/dinkla/raytracer"
         extractFileName(fileName, directory, "/") shouldBe "examples/NewWorld3.kt"
     }
-}
+})
