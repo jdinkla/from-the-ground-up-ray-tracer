c1b9b2cadceffb604346f3ce9bb1fa3c96715a42
Jörn Dinkla <jdinkla@thoughtworks.com>
2022-09-16 16:24:23 +0200

    add Image

---
+++ src/linuxX64Main/kotlin/net/dinkla/raytracer/utilities/Image.kt
@@ -0,0 +1,14 @@
+package net.dinkla.raytracer.utilities
+
+actual class Image {
+    actual fun getRGB(x: Int, y: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    actual companion object {
+        actual fun readImage(filename: String): Image {
+            TODO("Not yet implemented")
+        }
+    }
+
+}
