f0b1e8869433ef3983c538b07a24b3cee478ca1f
Jörn Dinkla <joern@dinkla.net>
2022-09-16 21:42:21 +0200

    improve log

--- src/jsMain/kotlin/net/dinkla/raytracer/utilities/writeLogMessage.kt
+++ src/jsMain/kotlin/net/dinkla/raytracer/utilities/writeLogMessage.kt
@@ -1,5 +1,7 @@
 package net.dinkla.raytracer.utilities
 
+import kotlin.js.Date
+
 internal actual fun writeLogMessage(logLevel: Logger.LogLevel, message: String) {
-    console.log(logLevel, message)
+    console.log(Date(), logLevel.name, message)
 }
