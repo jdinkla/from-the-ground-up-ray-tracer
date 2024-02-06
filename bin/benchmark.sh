78a3e572a17580202c5ebe6f75a6a390e3ff1186
Jörn Dinkla <jdinkla@thoughtworks.com>
2024-02-06 17:17:45 +0100

    add benchmark script for reference

---
+++ bin/benchmark.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+java --version
+PAGER= git show --oneline -s
+#time ./gradlew run --args="--world=World42.kt --renderer=PARALLEL --resolution=2160p"
+time ./gradlew run --args="--world=World42.kt --renderer=FORK_JOIN --resolution=2160p"
