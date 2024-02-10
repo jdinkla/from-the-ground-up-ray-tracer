74577e81ce9a69b9571d3f944efd4fd9c8c5e0c1
Jörn Dinkla <jdinkla@thoughtworks.com>
2024-02-10 09:37:46 +0100

    remove texture related classes

--- src/commonMain/kotlin/net/dinkla/raytracer/textures/Texture.kt
+++
@@ -1,9 +0,0 @@
-package net.dinkla.raytracer.textures
-
-import net.dinkla.raytracer.colors.Color
-import net.dinkla.raytracer.hits.IShade
-
-abstract class Texture {
-
-    abstract fun getColor(sr: IShade): Color
-}
