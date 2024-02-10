4d19e447bf6528243d88b47e4b6f3e9a391bf337
Jörn Dinkla <jdinkla@thoughtworks.com>
2024-02-10 12:53:50 +0100

    clean up

--- src/jvmMain/kotlin/net/dinkla/raytracer/Main.kt
+++ src/jvmMain/kotlin/net/dinkla/raytracer/Main.kt
@@ -1,17 +1,17 @@
 package net.dinkla.raytracer
 import kotlinx.coroutines.runBlocking
 import net.dinkla.raytracer.examples.worldMap
-import net.dinkla.raytracer.utilities.outputPngFileName
 import net.dinkla.raytracer.renderer.Renderer
 import net.dinkla.raytracer.tracers.Tracers
 import net.dinkla.raytracer.utilities.CommandLine
 import net.dinkla.raytracer.utilities.Logger
 import net.dinkla.raytracer.utilities.Resolution
 import net.dinkla.raytracer.utilities.Resolution.Companion.resolutions
+import net.dinkla.raytracer.utilities.outputPngFileName
 import net.dinkla.raytracer.world.Context
 import net.dinkla.raytracer.world.Render
 
-class CommandLineJvm(
+class Main(
     worlds: Collection<String>,
     tracers: Collection<Tracers>,
     renderers: Collection<Renderer>,
@@ -19,14 +19,12 @@ class CommandLineJvm(
 ) : CommandLine(worlds, tracers, renderers, resolutions) {
     override fun render(context: Context) {
         runBlocking {
-            Render.render(world, outputPngFileName(world), context)
+            Render.render(world, "../${outputPngFileName(world)}", context)
         }
     }
 }
 
 fun main(args: Array<String>) {
     Logger.info("From-the-ground-up-raytracer on JVM")
-    val renderers = Renderer.values().toList()
-    val tracers = Tracers.values().toList()
-    CommandLineJvm(worldMap.keys, tracers, renderers, resolutions).main(args)
+    Main(worldMap.keys, Tracers.entries, Renderer.entries, resolutions).main(args)
 }
