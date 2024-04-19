3775e7e07e310600eca4aff526e9537620eb40d4
Jörn Dinkla <jdinkla@thoughtworks.com>
2024-04-19 10:27:27 +0200

    use standard hash

--- src/commonMain/kotlin/net/dinkla/raytracer/utilities/Hash.kt
+++ src/commonMain/kotlin/net/dinkla/raytracer/utilities/Hash.kt
@@ -1,10 +1,5 @@
 package net.dinkla.raytracer.utilities
 
-fun <T> T.hash(vararg objects: Any): Int {
-    val prime = 31
-    var result = 1
-    objects.forEach {
-        result = prime * result + it.hashCode()
-    }
-    return result
-}
+import java.util.Objects
+
+fun <T> T.hash(vararg objects: Any): Int = Objects.hash(*objects)
