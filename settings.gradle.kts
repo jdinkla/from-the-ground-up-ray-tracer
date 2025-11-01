738a561e1594d8f4494fb8af18160dad375e3b21
Jörn Dinkla <jdinkla@thoughtworks.com>
2025-11-01 15:19:20 +0100

    upgrade versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
 
 plugins {
     id("de.fayard.refreshVersions") version "0.60.5"
+////                            # available:"0.60.6"
 }
 
 rootProject.name = "from-the-ground-up-ray-tracer"
